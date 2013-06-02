615fde5c7ce344a39e3d4128eab780c07e1f5faa
Vishal <Vishal.Patel@nab.com.au>
2013-06-03 08:58:44 +1000

    Initial Commit

---
+++ SampleiOSGame/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  SampleiOSGameAppController.h
+//  SampleiOSGame
+//
+//  Created by Vishal on 3/06/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ SampleiOSGame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  SampleiOSGameAppController.h
+//  SampleiOSGame
+//
+//  Created by Vishal on 3/06/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
