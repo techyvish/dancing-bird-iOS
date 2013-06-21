71ba5320e7a1a7c8623b26303768394dbb35ddb6
Vishal <Vishal.Patel@nab.com.au>
2013-06-21 17:41:34 +1000

    Added Cloud emitter

---
+++ SampleiOSGame/CloudEmitter.cpp
@@ -0,0 +1,9 @@
+//
+//  CloudEmitter.cpp
+//  SampleiOSGame
+//
+//  Created by Vishal on 19/06/13.
+//
+//
+
+#include "CloudEmitter.h"
---
+++ SampleiOSGame/CloudEmitter.h
@@ -0,0 +1,14 @@
+//
+//  CloudEmitter.h
+//  SampleiOSGame
+//
+//  Created by Vishal on 19/06/13.
+//
+//
+
+#ifndef __SampleiOSGame__CloudEmitter__
+#define __SampleiOSGame__CloudEmitter__
+
+#include <iostream>
+
+#endif /* defined(__SampleiOSGame__CloudEmitter__) */
